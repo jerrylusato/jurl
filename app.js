4f49cbdc488cf1e7edbbbbb8a798d72cdda7df09
jerrylusato <jeremiahlusato@gmail.com>
2022-06-10 09:32:37 +0300

    use express json middleware

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express()
 
 const Url = require("./models/url");
 
+app.use(express.json())
 app.set("view engine", "ejs")
 app.use(express.static("public"))
 app.use(express.urlencoded({ extended: false }))
